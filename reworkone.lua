c4c6955c32515d72a5ff93f4f28e503aab4991ab
tonplamm <98035032+tonplamm@users.noreply.github.com>
2022-09-17 07:54:53 +0700

    Add files via upload

--- reworkone.lua
+++ reworkone.lua
@@ -125,178 +125,182 @@ end)
 
 --MyAccount:SetDescription('Lv : ' ..LeveltoShow ..' Money : ' ..Money ..' ' ..longtei)
 
-while true do
-    --check mastery
-    OWN_DF = game:GetService("Players").LocalPlayer.Data.DevilFruit.Value
-    MAS_DF = game:GetService("Players").LocalPlayer.Backpack[OWN_DF].Level.Value
+spawn(function()
+    while wait() do
+        pcall(function()
+            --check mastery
+            OWN_DF = game:GetService("Players").LocalPlayer.Data.DevilFruit.Value
+            MAS_DF = game:GetService("Players").LocalPlayer.Backpack[OWN_DF].Level.Value
 
-    if OWN_DF == 'Kilo-Kilo' then
-        MAS_DF_CHECK = 15
-    elseif OWN_DF == 'Spin-Spin' then
-        MAS_DF_CHECK = 15
-    elseif OWN_DF == 'Chop-Chop' then
-        MAS_DF_CHECK = 50
-    elseif OWN_DF == 'Spring-Spring' then
-        MAS_DF_CHECK = 80
-    elseif OWN_DF == 'Bomb-Bomb' then
-        MAS_DF_CHECK = 125
-    elseif OWN_DF == 'Smoke-Smoke' then
-        MAS_DF_CHECK = 50
-    elseif OWN_DF == 'Spike-Spike' then
-        MAS_DF_CHECK = 90
-    elseif OWN_DF == 'Flame-Flame' then
-        MAS_DF_CHECK = 100
-    elseif OWN_DF == 'Bird-Bird: Falcon' then
-        MAS_DF_CHECK = 120
-    elseif OWN_DF == 'Ice-Ice' then
-        MAS_DF_CHECK = 100
-    elseif OWN_DF == 'Sand-Sand' then
-        MAS_DF_CHECK = 120
-    elseif OWN_DF == 'Dark-Dark' then
-        MAS_DF_CHECK = 110
-    elseif OWN_DF == 'Revive-Revive' then
-        MAS_DF_CHECK = 110
-    elseif OWN_DF == 'Diamond-Diamond' then
-        MAS_DF_CHECK = 110
-    elseif OWN_DF == 'Light-Light' then
-        MAS_DF_CHECK = 110
-    elseif OWN_DF == 'Love-Love' then
-        MAS_DF_CHECK = 100
-    elseif OWN_DF == 'Rubber-Rubber' then
-        MAS_DF_CHECK = 150
-    elseif OWN_DF == 'Barrier-Barrier' then
-        MAS_DF_CHECK = 130
-    elseif OWN_DF == 'Magma-Magma' then
-        MAS_DF_CHECK = 140
-    elseif OWN_DF == 'Door-Door' then
-        MAS_DF_CHECK = 200
-    elseif OWN_DF == 'Quake-Quake' then
-        MAS_DF_CHECK = 150
-    elseif OWN_DF == 'Human-Human: Buddha' then
-        MAS_DF_CHECK = 150
-    elseif OWN_DF == 'String-String' then
-        MAS_DF_CHECK = 225
-    elseif OWN_DF == 'Bird-Bird: Phoenix' then
-        MAS_DF_CHECK = 250
-    elseif OWN_DF == 'Rumble-Rumble' then
-        MAS_DF_CHECK = 250
-    elseif OWN_DF == 'Paw-Paw' then
-        MAS_DF_CHECK = 300
-    elseif OWN_DF == 'Gravity-Gravity' then
-        MAS_DF_CHECK = 300
-    elseif OWN_DF == 'Dough-Dough' then
-        MAS_DF_CHECK = 300
-    elseif OWN_DF == 'Shadow-Shadow' then
-        MAS_DF_CHECK = 300
-    elseif OWN_DF == 'Venom-Venom' then
-        MAS_DF_CHECK = 300
-    elseif OWN_DF == 'Control-Control' then
-        MAS_DF_CHECK = 350
-    elseif OWN_DF == 'Soul-Soul' then
-        MAS_DF_CHECK = 350
-    elseif OWN_DF == 'Dragon-Dragon' then
-        MAS_DF_CHECK = 350
-    elseif OWN_DF == 'Leopard-Leopard' then
-        MAS_DF_CHECK = 300
+            if OWN_DF == 'Kilo-Kilo' then
+                MAS_DF_CHECK = 15
+            elseif OWN_DF == 'Spin-Spin' then
+                MAS_DF_CHECK = 15
+            elseif OWN_DF == 'Chop-Chop' then
+                MAS_DF_CHECK = 50
+            elseif OWN_DF == 'Spring-Spring' then
+                MAS_DF_CHECK = 80
+            elseif OWN_DF == 'Bomb-Bomb' then
+                MAS_DF_CHECK = 125
+            elseif OWN_DF == 'Smoke-Smoke' then
+                MAS_DF_CHECK = 50
+            elseif OWN_DF == 'Spike-Spike' then
+                MAS_DF_CHECK = 90
+            elseif OWN_DF == 'Flame-Flame' then
+                MAS_DF_CHECK = 100
+            elseif OWN_DF == 'Bird-Bird: Falcon' then
+                MAS_DF_CHECK = 120
+            elseif OWN_DF == 'Ice-Ice' then
+                MAS_DF_CHECK = 100
+            elseif OWN_DF == 'Sand-Sand' then
+                MAS_DF_CHECK = 120
+            elseif OWN_DF == 'Dark-Dark' then
+                MAS_DF_CHECK = 110
+            elseif OWN_DF == 'Revive-Revive' then
+                MAS_DF_CHECK = 110
+            elseif OWN_DF == 'Diamond-Diamond' then
+                MAS_DF_CHECK = 110
+            elseif OWN_DF == 'Light-Light' then
+                MAS_DF_CHECK = 110
+            elseif OWN_DF == 'Love-Love' then
+                MAS_DF_CHECK = 100
+            elseif OWN_DF == 'Rubber-Rubber' then
+                MAS_DF_CHECK = 150
+            elseif OWN_DF == 'Barrier-Barrier' then
+                MAS_DF_CHECK = 130
+            elseif OWN_DF == 'Magma-Magma' then
+                MAS_DF_CHECK = 140
+            elseif OWN_DF == 'Door-Door' then
+                MAS_DF_CHECK = 200
+            elseif OWN_DF == 'Quake-Quake' then
+                MAS_DF_CHECK = 150
+            elseif OWN_DF == 'Human-Human: Buddha' then
+                MAS_DF_CHECK = 150
+            elseif OWN_DF == 'String-String' then
+                MAS_DF_CHECK = 225
+            elseif OWN_DF == 'Bird-Bird: Phoenix' then
+                MAS_DF_CHECK = 250
+            elseif OWN_DF == 'Rumble-Rumble' then
+                MAS_DF_CHECK = 250
+            elseif OWN_DF == 'Paw-Paw' then
+                MAS_DF_CHECK = 300
+            elseif OWN_DF == 'Gravity-Gravity' then
+                MAS_DF_CHECK = 300
+            elseif OWN_DF == 'Dough-Dough' then
+                MAS_DF_CHECK = 300
+            elseif OWN_DF == 'Shadow-Shadow' then
+                MAS_DF_CHECK = 300
+            elseif OWN_DF == 'Venom-Venom' then
+                MAS_DF_CHECK = 300
+            elseif OWN_DF == 'Control-Control' then
+                MAS_DF_CHECK = 350
+            elseif OWN_DF == 'Soul-Soul' then
+                MAS_DF_CHECK = 350
+            elseif OWN_DF == 'Dragon-Dragon' then
+                MAS_DF_CHECK = 350
+            elseif OWN_DF == 'Leopard-Leopard' then
+                MAS_DF_CHECK = 300
+            end
+            
+            if MAS_DF >= MAS_DF_CHECK then
+                Mastery_FULL = true
+                TextMasDF.Text = 'Full Mas'
+                TextMasDF.TextColor3 = GREEN
+            else
+                TextMasDF.Text = 'Mas not enough'
+                TextMasDF.TextColor3 = RED
+            end
+
+            -- check Money --
+            if game:GetService("Players").LocalPlayer.Data.Beli.Value >= 15000000 then
+                MONEY_FULL = true
+            end
+
+            if Check_Item('Buddy Sword',Can_Found) == 'Found' then
+                Have_Buddy = true
+                game:GetService("CoreGui").hehe.Textbuddy.Text = 'Have Buddy'
+                game:GetService("CoreGui").hehe.Textbuddy.TextColor3 = GREEN
+            else
+                Have_Buddy = false
+                game:GetService("CoreGui").hehe.Textbuddy.Text = 'Dont have Buddy'
+                game:GetService("CoreGui").hehe.Textbuddy.TextColor3 = RED
+            end
+
+            if Check_Item('Yama',Can_Found) == 'Found' then
+                Have_Yama = true
+                game:GetService("CoreGui").hehe.TextYama.Text = 'Have Yama'
+                game:GetService("CoreGui").hehe.TextYama.TextColor3 = GREEN
+            else
+                Have_Yama = false
+                game:GetService("CoreGui").hehe.TextYama.Text = 'Dont have Yama'
+                game:GetService("CoreGui").hehe.TextYama.TextColor3 = RED
+            end
+
+            if Check_Item('Hallow Scythe',Can_Found) == 'Found' then
+                Have_Hallow_Scythe = true
+                game:GetService("CoreGui").hehe.TextHallowScythe.Text = 'Have Hallow Scythe'
+                game:GetService("CoreGui").hehe.TextHallowScythe.TextColor3 = GREEN
+            else
+                Have_Hallow_Scythe = false
+                game:GetService("CoreGui").hehe.TextHallowScythe.Text = 'Dont have Hallow Scythe'
+                game:GetService("CoreGui").hehe.TextHallowScythe.TextColor3 = RED
+            end
+
+            if Check_Item('Dark Coat',Can_Found) == 'Found' then
+                Have_Dark_Coat = true
+                game:GetService("CoreGui").hehe.TextDarkCoat.Text = 'Have Dark Coat'
+                game:GetService("CoreGui").hehe.TextDarkCoat.TextColor3 = GREEN
+            else
+                Have_Dark_Coat = false
+                game:GetService("CoreGui").hehe.TextDarkCoat.Text = 'Dont have Dark Coat'
+                game:GetService("CoreGui").hehe.TextDarkCoat.TextColor3 = RED
+            end
+
+            if Check_Item('Dark Dagger',Can_Found) == 'Found' then
+                Have_Dark_Dagger = true
+                game:GetService("CoreGui").hehe.TextDarkDagger.Text = 'Have Dark Dagger'
+                game:GetService("CoreGui").hehe.TextDarkDagger.TextColor3 = GREEN
+            else
+                Have_Dark_Dagger = false
+                game:GetService("CoreGui").hehe.TextDarkDagger.Text = 'Dont have Dark Dagger'
+                game:GetService("CoreGui").hehe.TextDarkDagger.TextColor3 = RED
+            end
+
+            if Check_Item('Tushita',Can_Found) == 'Found' then
+                Have_Tushita = true
+                game:GetService("CoreGui").hehe.TextTushita.Text = 'Have Tushita'
+                game:GetService("CoreGui").hehe.TextTushita.TextColor3 = GREEN
+            else
+                Have_Tushita = false
+                game:GetService("CoreGui").hehe.TextTushita.Text = 'Dont have Tushita'
+                game:GetService("CoreGui").hehe.TextTushita.TextColor3 = RED
+            end
+
+            wait(10)
+
+            --refresh
+            game:GetService("CoreGui").hehe.TextMasDF.Text = 'Refresh..'
+            game:GetService("CoreGui").hehe.TextMasDF.TextColor3 = WHITE
+
+            game:GetService("CoreGui").hehe.Textbuddy.Text = 'Refresh..'
+            game:GetService("CoreGui").hehe.Textbuddy.TextColor3 = WHITE
+
+            game:GetService("CoreGui").hehe.TextYama.Text = 'Refresh..'
+            game:GetService("CoreGui").hehe.TextYama.TextColor3 = WHITE
+
+            game:GetService("CoreGui").hehe.TextHallowScythe.Text = 'Refresh..'
+            game:GetService("CoreGui").hehe.TextHallowScythe.TextColor3 = WHITE
+
+            game:GetService("CoreGui").hehe.TextDarkCoat.Text = 'Refresh..'
+            game:GetService("CoreGui").hehe.TextDarkCoat.TextColor3 = WHITE
+
+            game:GetService("CoreGui").hehe.TextDarkDagger.Text = 'Refresh..'
+            game:GetService("CoreGui").hehe.TextDarkDagger.TextColor3 = WHITE
+
+            game:GetService("CoreGui").hehe.TextTushita.Text = 'Refresh..'
+            game:GetService("CoreGui").hehe.TextTushita.TextColor3 = WHITE
+
+            wait(1)
+        end)
     end
-       
-    if MAS_DF >= MAS_DF_CHECK then
-        Mastery_FULL = true
-        TextMasDF.Text = 'Full Mas'
-        TextMasDF.TextColor3 = GREEN
-    else
-        TextMasDF.Text = 'Mas not enough'
-        TextMasDF.TextColor3 = RED
-    end
-
-    -- check Money --
-    if game:GetService("Players").LocalPlayer.Data.Beli.Value >= 15000000 then
-        MONEY_FULL = true
-    end
-
-    if Check_Item('Buddy Sword',Can_Found) == 'Found' then
-        Have_Buddy = true
-        game:GetService("CoreGui").hehe.Textbuddy.Text = 'Have Buddy'
-        game:GetService("CoreGui").hehe.Textbuddy.TextColor3 = GREEN
-    else
-        Have_Buddy = false
-        game:GetService("CoreGui").hehe.Textbuddy.Text = 'Dont have Buddy'
-        game:GetService("CoreGui").hehe.Textbuddy.TextColor3 = RED
-    end
-
-    if Check_Item('Yama',Can_Found) == 'Found' then
-        Have_Yama = true
-        game:GetService("CoreGui").hehe.TextYama.Text = 'Have Yama'
-        game:GetService("CoreGui").hehe.TextYama.TextColor3 = GREEN
-    else
-        Have_Yama = false
-        game:GetService("CoreGui").hehe.TextYama.Text = 'Dont have Yama'
-        game:GetService("CoreGui").hehe.TextYama.TextColor3 = RED
-    end
-
-    if Check_Item('Hallow Scythe',Can_Found) == 'Found' then
-        Have_Hallow_Scythe = true
-        game:GetService("CoreGui").hehe.TextHallowScythe.Text = 'Have Hallow Scythe'
-        game:GetService("CoreGui").hehe.TextHallowScythe.TextColor3 = GREEN
-    else
-        Have_Hallow_Scythe = false
-        game:GetService("CoreGui").hehe.TextHallowScythe.Text = 'Dont have Hallow Scythe'
-        game:GetService("CoreGui").hehe.TextHallowScythe.TextColor3 = RED
-    end
-
-    if Check_Item('Dark Coat',Can_Found) == 'Found' then
-        Have_Dark_Coat = true
-        game:GetService("CoreGui").hehe.TextDarkCoat.Text = 'Have Dark Coat'
-        game:GetService("CoreGui").hehe.TextDarkCoat.TextColor3 = GREEN
-    else
-        Have_Dark_Coat = false
-        game:GetService("CoreGui").hehe.TextDarkCoat.Text = 'Dont have Dark Coat'
-        game:GetService("CoreGui").hehe.TextDarkCoat.TextColor3 = RED
-    end
-
-    if Check_Item('Dark Dagger',Can_Found) == 'Found' then
-        Have_Dark_Dagger = true
-        game:GetService("CoreGui").hehe.TextDarkDagger.Text = 'Have Dark Dagger'
-        game:GetService("CoreGui").hehe.TextDarkDagger.TextColor3 = GREEN
-    else
-        Have_Dark_Dagger = false
-        game:GetService("CoreGui").hehe.TextDarkDagger.Text = 'Dont have Dark Dagger'
-        game:GetService("CoreGui").hehe.TextDarkDagger.TextColor3 = RED
-    end
-
-    if Check_Item('Tushita',Can_Found) == 'Found' then
-        Have_Tushita = true
-        game:GetService("CoreGui").hehe.TextTushita.Text = 'Have Tushita'
-        game:GetService("CoreGui").hehe.TextTushita.TextColor3 = GREEN
-    else
-        Have_Tushita = false
-        game:GetService("CoreGui").hehe.TextTushita.Text = 'Dont have Tushita'
-        game:GetService("CoreGui").hehe.TextTushita.TextColor3 = RED
-    end
-
-    wait(10)
-
-    --refresh
-    game:GetService("CoreGui").hehe.TextMasDF.Text = 'Refresh..'
-    game:GetService("CoreGui").hehe.TextMasDF.TextColor3 = WHITE
-
-    game:GetService("CoreGui").hehe.Textbuddy.Text = 'Refresh..'
-    game:GetService("CoreGui").hehe.Textbuddy.TextColor3 = WHITE
-
-    game:GetService("CoreGui").hehe.TextYama.Text = 'Refresh..'
-    game:GetService("CoreGui").hehe.TextYama.TextColor3 = WHITE
-
-    game:GetService("CoreGui").hehe.TextHallowScythe.Text = 'Refresh..'
-    game:GetService("CoreGui").hehe.TextHallowScythe.TextColor3 = WHITE
-
-    game:GetService("CoreGui").hehe.TextDarkCoat.Text = 'Refresh..'
-    game:GetService("CoreGui").hehe.TextDarkCoat.TextColor3 = WHITE
-
-    game:GetService("CoreGui").hehe.TextDarkDagger.Text = 'Refresh..'
-    game:GetService("CoreGui").hehe.TextDarkDagger.TextColor3 = WHITE
-
-    game:GetService("CoreGui").hehe.TextTushita.Text = 'Refresh..'
-    game:GetService("CoreGui").hehe.TextTushita.TextColor3 = WHITE
-
-    wait(1)
-end
+end)
